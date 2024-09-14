de100ab72e6e605ed2d68541dab2b9b060a1a4d6
Praveen De Silva <desilvabkp.23@uom.lk>
2024-09-14 15:43:35 +0530

    insertion

--- Sorting.py
+++ Sorting.py
@@ -1,6 +1,7 @@
 # --------------------
 # Bubble(Sinking) Sort
 # --------------------
+
 #   * rapidly compair each pair of adjacent items
 #     and swap them if they are wrong order.
 #   * timeComplexity  : O(n^2)
@@ -17,11 +18,59 @@ def bubbleSort(array):
         
 
 ##arr1 = [7,2,1,6,5,4,3]
-##
 ##bubbleSort(arr1)
 ##print(arr1)
 
 
-# --------------------
-# Bubble(Sinking) Sort
-# --------------------
+# --------------
+# Selection Sort
+# --------------
+
+#   * rapidly find the minimum of unsorted part
+#     and moveit to the sorted part.
+#   * timeComplexity  : O(n^2)
+#     spaceComplexity : O(1)
+
+def selectionSort(array):
+    '''sort the array using Selection Sort Algorithm'''
+    size = len(array)
+
+    for j in range(size-1):
+        tempMinIndex = j
+
+        for i in range(j+1, size):
+            if array[i]<array[tempMinIndex]:
+                tempMinIndex = i
+                
+        array[j], array[tempMinIndex] = array[tempMinIndex], array[j]
+    
+##arr1 = [7,6,5,1,2,3,4]
+##selectionSort(arr1)
+##print(arr1)
+
+
+# --------------
+# Insertion Sort
+# --------------
+
+#   * take the first element of the unsorted part and insert it
+#     at the correct position of the sorted part.
+#   * timeComplexity  : O(n^2)
+#     spaceComplexity : O(1)
+
+def insertionSort(array):
+    size = len(array)
+    
+    for j in range(size):
+        for i in range(j):
+            if array[i]>array[j]:
+                tempInsert = array[j]
+                array.pop(j)
+                array.insert(i, tempInsert)
+                break
+            print(array[i], end=', ')
+        print()
+
+arr1 = [7,6,5,1,2,3,4]
+insertionSort(arr1)
+print(arr1)
