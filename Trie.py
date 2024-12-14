3ba3a54ef7c2e92c4dd6248134f3b703ba4e55a0
Praveen De Silva <desilvabkp.23@uom.lk>
2024-12-14 23:11:13 +0530

    Trie_search

--- Trie.py
+++ Trie.py
@@ -18,9 +18,42 @@ class Trie:
                 node = TrieNode()
                 current.children[ch] = node
             current = node
+
         current.endOfString = True
         return "Inserted successfully!"
 
+    def searchNode(self, word):
+        current = self.root
+
+        for ch in word:
+            node = current.children.get(ch)
+
+            if node == None:
+                return False
+            current = node
+
+        if current.endOfString: # check whether there is a end
+            return True
+        return False
+
+    # My Method :
+    # def searchString(self, word):
+    #     current = self.root
+
+    #     for ch in word:
+    #         if ch not in current.children:
+    #             break
+    #         current = current.children[ch]
+    #     else:
+    #         if current.endOfString:
+    #             return f"'{word}' can found."
+    #     return f"'{word}' can NOT found."
+
+    
+
+
 trie = Trie()
 trie.insertString("APP")
-print(trie.insertString("API"))
+trie.insertString("API")
+
+print(trie.searchString('I'))
